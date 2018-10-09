abb463de150b72a9ec3b140e4d5f3e0403301723
lfzhu <zhulinfeng@yunjingit.com>
2018-10-09 23:04:42 +0800

    architype of project

---
+++ frame/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='rango-frame',
+      version='0.1',
+      packages=find_packages(),
+      include_package_data=True,
+      author='zhulinfeng',
+      author_email='zhulinfeng@yunjingit.com',
+      license='N/A',
+      description='A restful django framework, wrapped all common features',
+      url='http://www.yunjingit.com/',
+      long_description='',
+      platforms=['Any', ],
+      install_requires=())
