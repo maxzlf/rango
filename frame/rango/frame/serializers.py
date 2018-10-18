fb64013521423834f3d7b4886a8f866ead00b844
zhulinfeng <zhulinfeng@yunjingit.com>
2018-10-18 13:53:32 +0800

    Writting self defined serializers

---
+++ frame/rango/frame/serializers.py
@@ -0,0 +1,9 @@
+from django.forms import Field, BooleanField, NullBooleanField, CharField
+from django.forms import IntegerField, FloatField, DecimalField
+from django.forms import DateField, TimeField, DateTimeField
+from django.forms import DurationField, RegexField, EmailField, FileField
+from django.forms import ImageField, URLField, ChoiceField, TypedChoiceField
+from django.forms import MultipleChoiceField, TypedMultipleChoiceField
+from django.forms import ComboField, MultiValueField, FileField
+from django.forms import SplitDateTimeField, GenericIPAddressField, SlugField
+from django.forms import UUIDField
